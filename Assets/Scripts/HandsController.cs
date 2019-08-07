726383ce801d87644c4171caa6ef3ec96896fd40
C.Rondeau <rondeau.clement@gmail.com>
2019-08-07 22:26:36 +0200

    + hand system

--- Assets/Scripts/HandsController.cs
+++ Assets/Scripts/HandsController.cs
@@ -8,18 +8,69 @@ public class HandsController : MonoBehaviour
     [SerializeField] private Transform m_leftHand;
     [SerializeField] private Transform m_rightHand;
 
+    private EHand m_last = EHand.NONE;
+
+    private enum EHand
+    {
+        NONE = 0,
+        LEFT = 1,
+        RIGHT = 2
+    }
+
+    [SerializeField] private GameObject[] m_objects;
+    private void DebugGrab()
+    {
+        GameObject go = Instantiate(m_objects[Random.Range(0, m_objects.Length)]);
+        GrabNewItem(go);
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.O))
+        {
+            DebugGrab();
+        }
+    }
+
     public void GrabNewItem(GameObject p_item)
     {
+        Transform hand = GetNextHand();
+        if (!IsHandFree(hand))
+        {
+            // Throw object
+            GameObject go = hand.GetChild(0).gameObject;
+            go.transform.parent = null;
+            go.AddComponent(typeof(ThrowEffect));
+        }
 
+        // Grab new object
+        p_item.transform.parent = hand;
+        p_item.transform.localPosition = Vector2.zero;
+
+        //TODO: Show arms if objects
     }
 
-    private bool IsLeftHandAvailable()
+    private bool IsHandFree(Transform p_hand)
     {
-        return m_leftHand.childCount == 0;
+        return p_hand.childCount == 0;
     }
 
-    private bool IsRightHandAvailable()
+    private Transform GetNextHand()
     {
-        return m_rightHand.childCount == 0;
+        if (m_last == EHand.NONE)
+        {
+            m_last = EHand.LEFT;
+            return m_leftHand;
+        }
+        else if (m_last == EHand.LEFT)
+        {
+            m_last = EHand.RIGHT;
+            return m_rightHand;
+        }
+        else
+        {
+            m_last = EHand.LEFT;
+            return m_leftHand;
+        }
     }
 }
---
+++ Assets/ThrowEffect.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowEffect : MonoBehaviour
+{
+    void Start()
+    {
+        Rigidbody2D rb2d = GetComponent<Rigidbody2D>();
+        if(rb2d == null)
+        {
+            rb2d = gameObject.AddComponent(typeof(Rigidbody2D)) as Rigidbody2D;
+        }
+
+        PlanetObjectController poc = GetComponent<PlanetObjectController>();
+        if(poc != null)
+            Destroy(poc);
+
+        rb2d.AddForce(new Vector2(Random.Range(-25f, 25f), 55f), ForceMode2D.Impulse);
+        rb2d.gravityScale = 5f;
+
+        Destroy(gameObject, 3.5f);
+    }
+}
