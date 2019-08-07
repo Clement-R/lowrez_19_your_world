85946a3bb9fbe97360d5a24cb9d8b0561335cdcc
Clement Rondeau <clement@gabsee.com>
2019-08-07 18:10:32 +0200

    _ add skeleton code for hands controller

---
+++ Assets/Scripts/HandsController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class HandsController : MonoBehaviour
+{
+    [SerializeField] private Transform m_leftHand;
+    [SerializeField] private Transform m_rightHand;
+
+    public void GrabNewItem(GameObject p_item)
+    {
+
+    }
+
+    private bool IsLeftHandAvailable()
+    {
+        return m_leftHand.childCount == 0;
+    }
+
+    private bool IsRightHandAvailable()
+    {
+        return m_rightHand.childCount == 0;
+    }
+}
